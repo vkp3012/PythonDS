7aeb84ccc127004069e2278911112b45ad0c639e
vkp3012 <vivekpatel301295@gmail.com>
2022-02-23 22:11:13 +0530

    print z completed

---
+++ Getting/printZ.py
@@ -0,0 +1,28 @@
+# Print Z
+# Easy
+
+#  1. You are required to print a 'z' of size 5 using '*'.                               
+
+# Constraints
+# No Constraints
+
+# Format
+# Input
+# There is no input
+
+# Output
+
+
+# Example
+# Sample Output
+# *****
+#    *
+#   *
+#  *
+# *****
+
+print("*****")
+print("   *")
+print("  *")
+print(" *")
+print("*****")
---
+++ Patterns/pattern10.py
@@ -0,0 +1,29 @@
+# Pattern 10
+# Easy
+
+# 1. You are given a number n.
+# 2. You've to create a pattern of * and separated by tab as shown in output format.
+
+# Constraints
+# 1 <= n <= 100
+#  Also, n is odd.
+
+# Format
+# Input
+# A number n
+
+# Output
+# pat101
+
+# Example
+# Sample Input
+
+# 5
+
+# Sample Output
+#       *	
+# 	*		*	
+# *				*	
+# 	*		*	
+# 		*
+
